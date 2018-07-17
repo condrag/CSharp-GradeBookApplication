03822ce45bd510e951fb1c2dc15e2a8177c4e953
condrag <condrag@gmail.com>
2018-07-16 22:36:06 -0500

    Get Ranked Letter Grade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -10,5 +10,33 @@ namespace GradeBook.GradeBooks
         {
             Type = Enums.GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
+            }
+            else
+            {
+                var totalGrade = 0.0;
+                foreach (var student in Students)
+                {
+                    totalGrade += student.AverageGrade;
+                }
+                var averageStutentGrade = totalGrade / Students.Count;
+                if (averageGrade >= .8 * averageStutentGrade)
+                    return 'A';
+                else if (averageGrade >= .6 * averageStutentGrade)
+                    return 'B';
+                else if (averageGrade >= .4 * averageStutentGrade)
+                    return 'C';
+                else if (averageGrade >= .2 * averageStutentGrade)
+                    return 'D';
+                else
+                    return 'F';
+
+            }
+        }
     }
 }
